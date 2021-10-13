2d704a7bb2032d97ed2757373e632e2e49b1521a
Joaonorr <joaonoroes.jn@gmail.com>
2021-10-13 20:09:19 -0300

    gamev2.0

--- game.cpp
+++ game.cpp
@@ -1,11 +1,93 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+void setSize(sf::Sprite& sprite, int width, int height) {
+    auto dim = sprite.getLocalBounds();
+    sprite.setScale((float) width / dim.width, (float) height / dim.height);
+}
+
+struct Entity{
+    int x {0}; //atributos
+    int y {0};
+    int step {0};
+    sf::Sprite sprite;
+
+    Entity(int x, int y, int step, sf::Texture& texture ):
+        x {x}, y {y}, step {step}, sprite(texture) {
+    }
+
+    void draw(sf::RenderWindow& window){
+        this->sprite.setPosition(x * step, y * step);
+        setSize(this->sprite, step, step);
+        window.draw(this->sprite);
+    }
+
+};
+
+struct Board {
+    int nc {0};
+    int nl {0};
+    int step {0};
+    sf::Sprite sprite;
+    sf::RectangleShape rect;
+    Board(int nc, int nl, int step, sf::Texture& texture) {
+        this->nc = nc;
+        this->nl = nl;
+        this->step = step;
+        this->sprite.setTexture(texture);
+        setSize(this->sprite, nc * step, nl * step);
+        this->sprite.setPosition(0, 0);
+
+        this->rect.setSize(sf::Vector2f(step, step));
+        this->rect.setFillColor(sf::Color::Transparent);
+        this->rect.setOutlineColor(sf::Color::Black);
+        this->rect.setOutlineThickness(2);
+    }
+void draw(sf::RenderWindow& window){
+    window.draw(this->sprite);
+}
+
+};
+
+void moveEntity(sf::Keyboard::Key key, Entity& entity, std::vector<sf::Keyboard::Key> move_keys) {
+    if( key == move_keys[0]){
+        entity.x--;
+    }
+    else if( key == move_keys[1]){
+        entity.y--;
+    }
+    else if( key == move_keys[2]){
+        entity.y++;
+    }
+    else if( key == move_keys[3]){
+        entity.x++;
+    }
+}
+
+
+sf::Texture loadTexture(std::string path) {
+    sf::Texture texture;
+    if(!texture.loadFromFile(path)){
+        std::cout << "ERROR LOADING TEXTURE" << "\n";
+    }
+
+    return texture;
+}
 
 int main(){
 
     // Laço para manter a janela aberta, Recebe 2 parâmetros: modo de vídeo e nome da janela.
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::Texture rocket_tex{ loadTexture("rocket.png")};
+    sf::Texture asteroid_tex{ loadTexture("asteroid.png")};
+    sf::Texture space_tex{ loadTexture("space.jpg")};
+
+    const int STEP {100};
+
+    Entity rocket( 1, 1, STEP, rocket_tex);
+    Entity asteroid( 1, 1, STEP, asteroid_tex);
+    Board board(7, 5, STEP, space_tex);
+
+    sf::RenderWindow window(sf::VideoMode(board.nc * STEP, board.nl * STEP), "SFML works!");
 
     // Enquanto a janela estiver aberta, limpe a tela e mostre o que houver nela.
     while(window.isOpen()){
@@ -14,17 +96,18 @@ int main(){
         while(window.pollEvent(event) ){ 
             if( event.type == sf::Event::Closed){
                 window.close();
-            }            
-        }
+            } else if(event.type == sf::Event::KeyPressed){
+                moveEntity(event.key.code, rocket, {sf::Keyboard::A, sf::Keyboard::W, sf::Keyboard::S, sf::Keyboard::D});
+                moveEntity(event.key.code, asteroid, {sf::Keyboard::Left, sf::Keyboard::Up, sf::Keyboard::Down, sf::Keyboard::Right});
 
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::White);
-        shape.setPosition(300, 300);
-        shape.setOutlineColor(sf::Color(21, 43, 200));
+            }  
+        }
 
         //módulo de atualizações e pintura
         window.clear();
-        window.draw(shape);
+        board.draw(window);
+        rocket.draw(window);
+        asteroid.draw(window);
         window.display();
     }
 
